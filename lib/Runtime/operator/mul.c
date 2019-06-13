7ec5d8231f61833aea678e934577294093395915
Chen-Pang He <chen.pang.he@jdh8.org>
2019-06-13 18:25:44 +0800

    Broadcasted binary float operators

--- lib/Runtime/operator/mul.c
+++ lib/Runtime/operator/mul.c
@@ -1,23 +1,17 @@
-#include <onnc/Runtime/operator/mul.h>
+typedef float Scalar;
+typedef int Index;
 
-#include <stdint.h>
-#include <stdbool.h>
+#include "generic/binary.h"
 
-void ONNC_RUNTIME_mul_float(
-  void * restrict onnc_runtime_context
-  ,const float * restrict input_A
-  ,int32_t input_A_ndim, const int32_t * restrict input_A_dims
-  ,const float * restrict input_B
-  ,int32_t input_B_ndim, const int32_t * restrict input_B_dims
-  ,float * restrict output_C
-  ,int32_t output_C_ndim, const int32_t * restrict output_C_dims
-  
-) {
-	int32_t size = 1 ;
-	for(int32_t i = 0 ; i < input_A_ndim ; ++i){
-		size *= input_A_dims[i] ;
-	}
-	for(int32_t i = 0 ; i < size ; ++i){
-		output_C[i] = input_A[i] * input_B[i] ;
-	}
+static float _mul(float a, float b)
+{
+    return a * b;
+}
+
+void ONNC_RUNTIME_mul_float(void* restrict context,
+    const float* restrict A, int Adim, const int* restrict Ashape,
+    const float* restrict B, int Bdim, const int* restrict Bshape,
+    float* restrict C, int Cdim, const int* restrict Cshape)
+{
+    _binary(C, _mul, A, Ashape, Adim, B, Bshape, Bdim);
 }
