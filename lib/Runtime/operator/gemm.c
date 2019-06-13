d77591e93b065111e9c6a3ee2bdb57a04255b123
Chen-Pang He <chen.pang.he@jdh8.org>
2019-06-13 18:40:44 +0800

    Vectorization and correct broadcasting for gemm

--- lib/Runtime/operator/gemm.c
+++ lib/Runtime/operator/gemm.c
@@ -1,36 +1,21 @@
-#include <onnc/Runtime/operator/gemm.h>
+typedef float Scalar;
+typedef int Index;
 
-#include <stdint.h>
-#include <stdbool.h>
+#include "generic/broadcast.h"
+#include "generic/gemm.h"
+#include "generic/size.h"
 
-void ONNC_RUNTIME_gemm_float(
-  void * restrict onnc_runtime_context
-  ,const float * restrict input_A
-  ,int32_t input_A_ndim, const int32_t * restrict input_A_dims
-  ,const float * restrict input_B
-  ,int32_t input_B_ndim, const int32_t * restrict input_B_dims
-  ,const float * restrict input_C
-  ,int32_t input_C_ndim, const int32_t * restrict input_C_dims
-  ,float * restrict output_Y
-  ,int32_t output_Y_ndim, const int32_t * restrict output_Y_dims
-  ,float alpha
-  ,float beta
-  ,int32_t transA
-  ,int32_t transB
-) {
-  int num = transA ? input_A_dims[0] : input_A_dims[1];
-  for (int32_t i = 0 ; i < output_Y_dims[0] ; ++i) {
-    for (int32_t j = 0 ; j < output_Y_dims[1] ; ++j) {
-      output_Y[ i * output_Y_dims[1] + j ] = 0;
-      for (int32_t k = 0 ; k < num ; ++k) {
-        output_Y[ i * output_Y_dims[1] + j ] +=
-          input_A[ transA ? (k * input_A_dims[1] + i) :
-                            (i * input_A_dims[1] + k) ] *
-          input_B[ transB ? (j * input_B_dims[1] + k) :
-                            (k * input_B_dims[1] + j) ];
-      }
-      output_Y[ i * output_Y_dims[1] + j ] *= alpha;
-      output_Y[ i * output_Y_dims[1] + j ] += beta * input_C[ + j ];
-    }
-  }
+void ONNC_RUNTIME_gemm_float(void* restrict context,
+    const float* restrict A, int Adim, const int* restrict Ashape,
+    const float* restrict B, int Bdim, const int* restrict Bshape,
+    const float* restrict C, int Cdim, const int* restrict Cshape,
+    float* restrict Y, int Ydim, const int* restrict Yshape,
+    float alpha, float beta, int transA, int transB)
+{
+    _broadcast(Y, Yshape, Ydim, C, Cshape, Cdim);
+
+    for (int i = 0; i < _size(Yshape, Ydim); ++i)
+        Y[i] *= beta;
+
+    _gemm(Y, A, B, alpha, Yshape[0], Yshape[1], Ashape[!transA], transA, transB);
 }
