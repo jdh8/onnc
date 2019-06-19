7b36e682282df0b2d0c2192b89142d3f730578f6
Chen-Pang He <chen.pang.he@jdh8.org>
2019-06-20 00:52:01 +0800

    Move out stride manipulation as a microlibrary

--- tools/unittests/Runtime/TransposeTest.cpp
+++ tools/unittests/Runtime/TransposeTest.cpp
@@ -4,55 +4,12 @@ extern "C" {
 }
 #undef restrict
 
+#include "valarray.hpp"
 #include <skypat/skypat.h>
 #include <algorithm>
 #include <functional>
 #include <numeric>
 #include <array>
-#include <valarray>
-#include <cstring>
-
-template<std::size_t order>
-static std::array<std::int32_t, order> compute_strides(const std::array<std::int32_t, order>& shape)
-{
-    std::array<std::int32_t, order> result;
-
-    if (order > 0)
-        result[order - 1] = 1;
-
-    for (std::size_t i = order - 1; i > 0; --i)
-        result[i - 1] = shape[i] * result[i];
-
-    for (std::size_t i = 0; i < order; ++i)
-        result[i] *= shape[i] > 1;
-
-    return result;
-}
-
-template<typename T>
-static std::valarray<T> range(std::size_t end)
-{
-    std::valarray<T> result(end);
-
-    for (std::size_t i = 0; i < end; ++i)
-        result[i] = i;
-
-    return result;
-}
-
-template<typename T, typename Container>
-static std::valarray<T> make_valarray(const Container& x)
-{
-    std::valarray<T> result(x.size());
-    std::copy(std::begin(x), std::end(x), std::begin(result));
-    return result;
-}
-
-template<typename T>
-static bool equal(const std::valarray<T>& x, const std::valarray<T>& y)
-{
-    return x.size() == y.size() && !std::memcmp(&x[0], &y[0], x.size() * sizeof(T));
-}
 
 /* FIXME We pass `permutation` by value because `ONNC_RUNTIME_transpose_float`
  * takes (non-const) `int32_t*` as permutation list.  Discuss if changing to
@@ -67,10 +24,10 @@ static void test_permutedims(
     using std::size_t;
     using std::int32_t;
 
-    const std::valarray<size_t> permute = make_valarray<size_t>(permutation);
+    const std::valarray<size_t> permute = onnc::valarray::make<size_t>(permutation);
     const std::valarray<int32_t> permuted_shape = shape[permute];
     const std::valarray<size_t> permuted_strides = strides[permute];
-    const std::gslice transpose = { 0, make_valarray<size_t>(permuted_shape), permuted_strides };
+    const std::gslice transpose = { 0, onnc::valarray::make<size_t>(permuted_shape), permuted_strides };
     const std::valarray<float> reference = tensor[transpose];
 
     std::valarray<float> candidate(tensor.size());
@@ -78,10 +35,9 @@ static void test_permutedims(
     ONNC_RUNTIME_transpose_float(nullptr,
         &tensor[0], shape.size(), &shape[0],
         &candidate[0], permuted_shape.size(), &permuted_shape[0],
-        &permutation[0], permutation.size()
-    );
+        &permutation[0], permutation.size());
 
-    EXPECT_TRUE(equal(reference, candidate));
+    EXPECT_TRUE(onnc::valarray::equal(reference, candidate));
 }
 
 template<typename... Args>
@@ -92,19 +48,19 @@ static void test(Args... args)
 
     const size_t order = sizeof...(Args);
     std::array<int32_t, order> shape = { static_cast<int32_t>(args)... };
-    std::array<int32_t, order> strides = compute_strides(shape);
-    size_t size = std::accumulate(shape.begin(), shape.end(), static_cast<size_t>(1), std::multiplies<size_t>());
+    std::valarray<size_t> strides = onnc::valarray::strides(shape);
+    size_t size = std::accumulate(shape.cbegin(), shape.cend(), static_cast<size_t>(1), std::multiplies<size_t>());
 
-    const std::gslice identity = { 0, make_valarray<size_t>(shape), make_valarray<size_t>(strides) };
-    const std::valarray<float> tensor = range<float>(size);
+    const std::gslice identity = { 0, onnc::valarray::make<size_t>(shape), strides };
+    const std::valarray<float> tensor = onnc::valarray::range<float>(size);
     const std::valarray<float> candidate = tensor[identity];
 
-    EXPECT_TRUE(equal(tensor, candidate));
+    EXPECT_TRUE(onnc::valarray::equal(tensor, candidate));
 
-    std::valarray<int32_t> permutation = range<int32_t>(order);
+    std::valarray<int32_t> permutation = onnc::valarray::range<int32_t>(order);
 
     while (std::next_permutation(std::begin(permutation), std::end(permutation)))
-        test_permutedims(tensor, permutation, make_valarray<int32_t>(shape), make_valarray<size_t>(strides));
+        test_permutedims(tensor, permutation, onnc::valarray::make<int32_t>(shape), strides);
 }
 
 SKYPAT_F(Operator_Transpose, non_broadcast)
