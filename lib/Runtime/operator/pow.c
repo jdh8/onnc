9b8e94689852e93169acae898f47a881ce637627
Chen-Pang He <chen.pang.he@jdh8.org>
2019-06-23 21:54:11 +0800

    Fix broadcasting of operator Pow

--- lib/Runtime/operator/pow.c
+++ lib/Runtime/operator/pow.c
@@ -1,25 +1,15 @@
-#include <onnc/Runtime/operator/pow.h>
-
 #include <stdint.h>
-#include <stdbool.h>
+typedef int32_t ONNC_INDEX_TYPE;
+
+#include "generic/assign.h"
+#include "generic/binary.h"
 #include <math.h>
 
-void ONNC_RUNTIME_pow_float(
-  void * restrict onnc_runtime_context
-  ,const float * restrict input_X
-  ,int32_t input_X_ndim, const int32_t * restrict input_X_dims
-  ,const float * restrict input_Y
-  ,int32_t input_Y_ndim, const int32_t * restrict input_Y_dims
-  ,float * restrict output_Z
-  ,int32_t output_Z_ndim, const int32_t * restrict output_Z_dims
-  
-) {
-  int32_t size = 1;
-  for(int32_t dim = 0 ; dim < input_X_ndim ; dim++){
-      size *= input_X_dims[dim];
-  }
-
-  for(int32_t index = 0 ; index < size ; index++){
-      output_Z[index] = pow(input_X[index], input_Y[index]);
-  }
+void ONNC_RUNTIME_pow_float(void* restrict context,
+    const float* restrict A, int32_t Adim, const int32_t* restrict Ashape,
+    const float* restrict B, int32_t Bdim, const int32_t* restrict Bshape,
+    float* restrict C, int32_t Cdim, const int32_t* restrict Cshape)
+{
+    ONNC_ASSIGN(float, C, Cshape, Cdim, A, Ashape, Adim);
+    ONNC_BINARY(float, C, Cshape, Cdim, B, Bshape, Bdim, powf);
 }
