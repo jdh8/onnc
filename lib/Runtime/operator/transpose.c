2de2bf5af04ed4029bdbffb48e7abad93d228738
Chen-Pang He <chen.pang.he@jdh8.org>
2019-06-25 15:02:29 +0800

    Handle default arguments of operator Transpose

--- lib/Runtime/operator/transpose.c
+++ lib/Runtime/operator/transpose.c
@@ -6,7 +6,12 @@ typedef int32_t ONNC_INDEX_TYPE;
 void ONNC_RUNTIME_transpose_float(void* restrict context,
     const float* restrict X, int32_t Xdim, const int32_t* restrict Xshape,
     float* restrict Y, int32_t Ydim, const int32_t* restrict Yshape,
-    const int32_t* restrict permute, int32_t dim)
+    const int32_t* restrict permute, int32_t given)
 {
-    ONNC_TRANSPOSE(float, Y, X, permute, Xshape, Xdim);
+    int32_t reverse[Xdim];
+
+    for (int32_t i = 0; i < Xdim; ++i)
+        reverse[i] = Xdim - 1 - i;
+
+    ONNC_TRANSPOSE(float, Y, X, given && permute ? permute : reverse, Xshape, Xdim);
 }
