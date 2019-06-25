78f3cd9e086e33c7860fa43e6b31bcfb32a58c87
Chen-Pang He <chen.pang.he@jdh8.org>
2019-06-25 20:57:49 +0800

    Implement operator Expand

--- lib/Runtime/operator/expand.c
+++ lib/Runtime/operator/expand.c
@@ -1,16 +1,12 @@
-#include <onnc/Runtime/operator/expand.h>
-
 #include <stdint.h>
-#include <stdbool.h>
+typedef int32_t ONNC_INDEX_TYPE;
 
-void ONNC_RUNTIME_expand_float(
-  void * restrict onnc_runtime_context
-  ,const float * restrict input_input
-  ,int32_t input_input_ndim, const int32_t * restrict input_input_dims
-  ,const float * restrict input_shape
-  ,int32_t input_shape_ndim, const int32_t * restrict input_shape_dims
-  ,float * restrict output_output
-  ,int32_t output_output_ndim, const int32_t * restrict output_output_dims
-  
-) {
+#include "generic/assign.h"
+
+void ONNC_RUNTIME_expand_float(void* restrict context,
+    const float* restrict X, int32_t Xndim, const int32_t* restrict Xshape,
+    const float* restrict shape, int32_t shape_ndim, const int32_t* restrict shape_shape,
+    float* restrict Y, int32_t Yndim, const int32_t* restrict Yshape)
+{
+    ONNC_ASSIGN(float, Y, Yshape, Yndim, X, Xshape, Xndim);
 }
