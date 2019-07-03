91e0dee3c010231f21b125a18d28971e3d62ed4d
Chen-Pang He <chen.pang.he@jdh8.org>
2019-07-03 22:27:13 +0800

    Implement identity cast

--- lib/Runtime/operator/cast.c
+++ lib/Runtime/operator/cast.c
@@ -1,14 +1,13 @@
-#include <onnc/Runtime/operator/cast.h>
-
 #include <stdint.h>
-#include <stdbool.h>
+typedef int32_t ONNC_INDEX_TYPE;
 
-void ONNC_RUNTIME_cast_float(
-  void * restrict onnc_runtime_context
-  ,const float * restrict input_input
-  ,int32_t input_input_ndim, const int32_t * restrict input_input_dims
-  ,float * restrict output_output
-  ,int32_t output_output_ndim, const int32_t * restrict output_output_dims
-  ,int32_t to
-) {
+#include "generic/size.h"
+#include <string.h>
+
+void ONNC_RUNTIME_cast_float(void* restrict context,
+    const float* X, int32_t Xorder, const int32_t* restrict Xshape,
+    float* Y, int32_t Yorder, const int32_t* restrict Yshape,
+    int32_t to)
+{
+    memcpy(Y, X, onnc_size(Xshape, Xorder) * sizeof(float));
 }
