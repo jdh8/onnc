5474773b2cad62598e747d6f05e911aa61a122a9
Chen-Pang He <chen.pang.he@jdh8.org>
2019-07-09 17:24:21 +0800

    Fix ArgMax and refactor ArgMin accordingly

--- lib/Runtime/operator/argmin.c
+++ lib/Runtime/operator/argmin.c
@@ -1,59 +1,32 @@
-#include <onnc/Runtime/operator/argmin.h>
-
 #include <stdint.h>
-#include <float.h>
-#include <stdbool.h>
+typedef int32_t ONNC_INDEX_TYPE;
 
-static int32_t findIndex(int32_t index, int32_t axisDim, int32_t elementDistance){
-  return (index / (axisDim * elementDistance)) * elementDistance + index % elementDistance;
-}
+#include "generic/size.h"
 
-void ONNC_RUNTIME_argmin_float(
-  void * restrict onnc_runtime_context
-  ,const float * restrict input_data
-  ,int32_t input_data_ndim, const int32_t * restrict input_data_dims
-  ,float * restrict output_reduced
-  ,int32_t output_reduced_ndim, const int32_t * restrict output_reduced_dims
-  ,int32_t axis
-  ,int32_t keepdims
-) {
-  int32_t axisDim = input_data_dims[axis];
-
-  int32_t axisElementDistance = 1;
-  for(int32_t dim = axis + 1 ; dim < input_data_ndim ; dim++){
-    axisElementDistance *= input_data_dims[dim];
-  }
-
-  int32_t size = axisElementDistance;
-  for(int32_t dim = 0 ; dim <= axis ; dim++){
-    size *= input_data_dims[dim];
-  }
-
-  int32_t try = 0;
-  while(try < size){
-    /* Initialize try context*/
-    int32_t index = try;
-    float min = FLT_MAX;
-    int32_t axisIndex = 0, minIndex = 0;
-    while(axisIndex < axisDim){
-      if(input_data[index] < min){
-        min = input_data[index];
-        minIndex = axisIndex;
-      }
-      index += axisElementDistance;
-      axisIndex += 1;
+void ONNC_RUNTIME_argmin_float(void* restrict context,
+    const float* restrict x, int32_t order, const int32_t* shape,
+    float* restrict y, int32_t ndim, const int32_t* reduced,
+    int32_t axis, int32_t keepdims)
+{
+    if (!order) {
+        *y = 0;
+        return;
     }
 
-    /* fill the data to output */
-    int32_t fillIndex = findIndex(try, axisDim, axisElementDistance);
-    output_reduced[fillIndex] = minIndex;
+    int32_t depth = shape[axis];
+    int32_t rows = onnc_size(shape, axis);
+    int32_t cols = onnc_size(shape + axis + 1, order - axis - 1);
 
-    /* update round context */
-    if(try % axisElementDistance == axisElementDistance - 1){
-      try = ((try + 1) - axisElementDistance) + axisElementDistance * axisDim;
+    for (int32_t i = 0; i < rows; ++i) {
+        for (int32_t j = 0; j < cols; ++j) {
+            int32_t candidate = 0;
+            int32_t offset = i * depth * cols + j;
+
+            for (int32_t k = 1; k < depth; ++k)
+                if (x[candidate * cols + offset] > x[k * cols + offset])
+                    candidate = k;
+
+            y[i * cols + j] = candidate;
+        }
     }
-    else{
-      try += 1;
-    }
-  }
 }
