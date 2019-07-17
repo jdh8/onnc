018ff007e20734585579aac87ca9a573aa012909
Chen-Pang He <chen.pang.he@jdh8.org>
2019-07-17 16:45:40 +0800

    Refactor Split like Concat, its inverse

--- lib/Runtime/operator/split.c
+++ lib/Runtime/operator/split.c
@@ -1,85 +1,28 @@
-#include <onnc/Runtime/operator/split.h>
-
 #include <stdint.h>
-#include <stdbool.h>
+typedef int32_t ONNC_INDEX_TYPE;
 
-static void forLoop(
-    const float * restrict input_input, int32_t input_index,
-    int32_t input_input_ndim, const int32_t * restrict input_input_dims,
-    int32_t dimIndex, int32_t * restrict axisDistance,
-    int32_t axis, int32_t axisLower, int32_t axisHigher,
-    float * const * restrict output, int32_t output_row, int32_t * restrict output_col
-){
-  if(dimIndex == input_input_ndim){
-    int32_t col = *output_col;
-    output[output_row][col] = input_input[input_index];
-    *output_col = col + 1;
-    return;
-  }
-  /* set loop lower and uper when encountering speicific axis */
-  int32_t loopLower = 0, loopUper = input_input_dims[dimIndex];
-  if(dimIndex == axis){
-    loopLower = axisLower;
-    loopUper = axisHigher;
-  }
-  for(int32_t dim = loopLower ; dim < loopUper ; dim++){
-      forLoop(input_input, input_index + dim * axisDistance[dimIndex],
-              input_input_ndim, input_input_dims,
-              dimIndex + 1, axisDistance,
-              axis, axisLower, axisHigher,
-              output, output_row, output_col);
-  }
-}
+#include "generic/size.h"
+#include <string.h>
 
-void ONNC_RUNTIME_split_float(
-  void * restrict onnc_runtime_context
-  ,const float * restrict input_input
-  ,int32_t input_input_ndim, const int32_t * restrict input_input_dims
-  ,float * const * restrict output_outputs
-  ,int32_t output_outputs_ntensor
-  ,const int32_t * output_outputs_ndim, const int32_t * const * restrict output_outputs_dims
-  ,int32_t axis
-  ,int32_t * restrict split
-  ,int32_t number_of_split
-) {
-  /* initialize parameter following the spec */
-  int32_t output_split_number = output_outputs_ntensor;
-  int32_t output_split[output_split_number];
-  if(number_of_split){
-    for(int32_t index = 0 ; index < output_split_number ; index++){
-        output_split[index] = split[index];
+void ONNC_RUNTIME_split_float(void* restrict context,
+    const float* restrict input, int32_t ndim, const int32_t* restrict shape,
+    float* restrict* outputs, int32_t count, const int32_t* restrict ndims, const int32_t* restrict* shapes,
+    int32_t axis, const int32_t* split, int32_t nsplit)
+{
+    int32_t slices = onnc_size(shape, axis);
+    int32_t block = onnc_size(shape + axis + 1, ndim - axis - 1);
+
+    int32_t offsets[count];
+
+    offsets[0] = 0;
+
+    for (int32_t i = 0; i < count - 1; ++i)
+        offsets[i + 1] = offsets[i] + split[i];
+
+    for (int32_t i = 0; i < count; ++i) {
+        for (int32_t j = 0; j < slices; ++j) {
+            int32_t length = split[i] * block;
+            memcpy(outputs[i] + j * length, input + (offsets[i] + j * shape[axis]) * block, length * sizeof(float));
+        }
     }
-  }
-  else{
-    int32_t avg_split = input_input_dims[axis] / output_split_number;
-    for(int32_t index = 0 ; index < output_split_number ; index++){
-      output_split[index] = avg_split;
-    }
-  }
-
-  /* acculate index */
-  int32_t base = 0;
-  for(int32_t index = 0 ; index < output_split_number ; index++){
-    output_split[index] = base + output_split[index];
-    base = output_split[index];
-  }
-
-  /* calculate axisDistance */
-  int32_t axisDistance[input_input_ndim];
-  int32_t axis_base = axisDistance[input_input_ndim-1] = 1;
-  for(int32_t dim = input_input_ndim - 2  ; dim >= 0 ; dim--){
-    axisDistance[dim] = axis_base * input_input_dims[dim+1];
-    axis_base *= input_input_dims[dim];
-  }
-
-  int32_t split_base = 0;
-  for(int32_t splitIndex = 0 ; splitIndex < output_split_number ; splitIndex++){
-    int32_t output_col = 0;
-    forLoop(input_input, 0,
-            input_input_ndim, input_input_dims,
-            0, axisDistance,
-            axis, split_base, output_split[splitIndex],
-            output_outputs, splitIndex, &output_col);
-    split_base = output_split[splitIndex];
-  }
 }
